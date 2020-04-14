15fb5591b166cce16b585facd9501d255aef8bbb
dgorbunov <danny.gorbunov@gmail.com>
2020-04-14 16:32:54 -0400

    fully working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SplineTest.java
@@ -1,12 +1,25 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import android.view.animation.LinearInterpolator;
+
+import com.acmerobotics.roadrunner.drive.DriveSignal;
+import com.acmerobotics.roadrunner.followers.HolonomicPIDVAFollower;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.path.LineSegment;
+import com.acmerobotics.roadrunner.path.Path;
+import com.acmerobotics.roadrunner.path.PathBuilder;
+import com.acmerobotics.roadrunner.path.PathSegment;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryGenerator;
+import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
 import com.acmerobotics.roadrunner.util.Angle;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import java.util.List;
 
 /*
  * This is an example of a more complex path to really test the tuning.
@@ -17,38 +30,40 @@ public class SplineTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
+
         waitForStart();
 
         if (isStopRequested()) return;
 
-        Trajectory traj = drive.trajectoryBuilder(new Pose2d())
-                .splineTo(new Pose2d(25, 25, 0))
+        Vector2d v = new Vector2d(0, 15);
+
+
+        Path path = new PathBuilder(new Pose2d(0, 0, 0))
+                .splineTo(new Pose2d(15, 15, 0))
+                .lineTo(new Vector2d(30, 15))
                 .build();
+
+
+        DriveConstraints constraints = new DriveConstraints(20, 40, 80, 1, 2, 4);
+        Trajectory traj = TrajectoryGenerator.INSTANCE.generateTrajectory(path, constraints);
+
+//        drive.turn(Math.toRadians(45));
+//        Trajectory traj = drive.trajectoryBuilder(new Pose2d(0, 0))
+//                .lineTo(v)
+//                .lineTo(new Vector2d(0,-15))
+//                .build();
 
         drive.followTrajectory(traj);
 
-        sleep(500);
+//        drive.turn(Math.toRadians(-45));
 
-        Trajectory traj2 = drive.trajectoryBuilder(new Pose2d())
-                .splineTo(new Pose2d(0, 50, 0))
-                .build();
+        sleep(100);
 
-        drive.followTrajectory(traj2);
 
-        sleep(500);
-
-        drive.followTrajectory(
-                drive.trajectoryBuilder(new Pose2d(25, 50, Math.toRadians(180)), true)
-                        .splineTo(new Pose2d(25, 25, Math.toRadians(180)))
-                        .build()
-        );
-
-        sleep(500);
-
-        drive.followTrajectory(
-                drive.trajectoryBuilder(new Pose2d(25, 25, Math.toRadians(180)), true)
-                        .splineTo(new Pose2d(0, 0, Math.toRadians(180)))
-                        .build()
-        );
+//        drive.followTrajectory(
+//                drive.trajectoryBuilder(new Pose2d(25, 25, Math.toRadians(180)), true)
+//                        .splineTo(new Pose2d(0, 0, Math.toRadians(180)))
+//                        .build()
+//        );
     }
 }
